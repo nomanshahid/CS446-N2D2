eff8757d5ad322733af8a62fe4428a440298d180
Naveed Sufi <nhsufi@uwaterloo.ca>
2019-07-08 18:46:29 -0400

    Added toggles

--- app/src/main/java/dylandesrosier/glossa/Settings.java
+++ app/src/main/java/dylandesrosier/glossa/Settings.java
@@ -4,8 +4,10 @@ import android.content.pm.ActivityInfo;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Switch;
 
 public class Settings extends AppCompatActivity {
 
@@ -13,6 +15,37 @@ public class Settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        Switch time_switch = findViewById(R.id.timeSwitch);
+        Switch location_switch = findViewById(R.id.locationSwitch);
+
+        time_switch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean enabled = ((Switch) v).isChecked();
+                if (enabled) {
+                    // TODO: Disable location and enable time
+                    Switch location_switch = findViewById(R.id.locationSwitch);
+                    location_switch.setChecked(false);
+                } else {
+                    // TODO: Disable time and enable location
+                }
+            }
+        });
+
+        location_switch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean enabled = ((Switch) v).isChecked();
+                if (enabled) {
+                    // TODO: Disable time and enable location
+                    Switch time_switch = findViewById(R.id.timeSwitch);
+                    time_switch.setChecked(false);
+                } else {
+                    // TODO: Disable location and enable time
+                }
+            }
+        });
 
         // Set the status bar to be transparent and lock app to portrait
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
@@ -22,4 +55,6 @@ public class Settings extends AppCompatActivity {
 
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
+
+    // TODO: Add input validation
 }
