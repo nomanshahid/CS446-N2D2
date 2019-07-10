14c37981c1c3aab5cbe86a3fc05028e9542f0089
Naveed Sufi <nhsufi@uwaterloo.ca>
2019-07-10 16:50:11 -0400

    Added timepicker dialog

--- app/src/main/java/dylandesrosier/glossa/Settings.java
+++ app/src/main/java/dylandesrosier/glossa/Settings.java
@@ -1,6 +1,8 @@
 package dylandesrosier.glossa;
 
+import android.app.TimePickerDialog;
 import android.content.pm.ActivityInfo;
+import android.icu.util.Calendar;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,6 +10,8 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 public class Settings extends AppCompatActivity {
 
@@ -16,36 +20,89 @@ public class Settings extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        Switch time_switch = findViewById(R.id.timeSwitch);
-        Switch location_switch = findViewById(R.id.locationSwitch);
+        Switch timeSwitch = findViewById(R.id.timeSwitch);
+        Switch locationSwitch = findViewById(R.id.locationSwitch);
+        TextView startTimeText = findViewById(R.id.startTimeText);
+        TextView endTimeText = findViewById(R.id.endTimeText);
 
-        time_switch.setOnClickListener(new View.OnClickListener() {
+        // TODO: Get and populate previous settings
+
+        timeSwitch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 boolean enabled = ((Switch) v).isChecked();
+                TextView startTimeText = findViewById(R.id.startTimeText);
+                TextView endTimeText = findViewById(R.id.endTimeText);
+                Switch locationSwitch = findViewById(R.id.locationSwitch);
                 if (enabled) {
                     // TODO: Disable location and enable time
-                    Switch location_switch = findViewById(R.id.locationSwitch);
-                    location_switch.setChecked(false);
+                    locationSwitch.setChecked(false);
+                    startTimeText.setEnabled(true);
+                    endTimeText.setEnabled(true);
                 } else {
                     // TODO: Disable time and enable location
+                    startTimeText.setEnabled(false);
+                    endTimeText.setEnabled(false);
                 }
             }
         });
 
-        location_switch.setOnClickListener(new View.OnClickListener() {
+        locationSwitch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 boolean enabled = ((Switch) v).isChecked();
+                Switch timeSwitch = findViewById(R.id.timeSwitch);
                 if (enabled) {
                     // TODO: Disable time and enable location
-                    Switch time_switch = findViewById(R.id.timeSwitch);
-                    time_switch.setChecked(false);
+                    timeSwitch.setChecked(false);
                 } else {
                     // TODO: Disable location and enable time
                 }
             }
         });
+
+
+        // Open time picker on focus and click
+        startTimeText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView startTimeText = findViewById(R.id.startTimeText);
+                TimePickerDialog tp = createTimePicker(startTimeText);
+                tp.show();
+            }
+        });
+
+        startTimeText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                if (hasFocus) {
+                    TextView startTimeText = findViewById(R.id.startTimeText);
+                    TimePickerDialog tp = createTimePicker(startTimeText);
+                    tp.show();
+                }
+            }
+        });
+
+        endTimeText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView endTimeText = findViewById(R.id.endTimeText);
+                TimePickerDialog tp = createTimePicker(endTimeText);
+                tp.show();
+            }
+        });
+
+        endTimeText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                if (hasFocus) {
+                    TextView endTimeText = findViewById(R.id.endTimeText);
+                    TimePickerDialog tp = createTimePicker(endTimeText);
+                    tp.show();
+                }
+            }
+        });
+
 
         // Set the status bar to be transparent and lock app to portrait
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
@@ -56,5 +113,17 @@ public class Settings extends AppCompatActivity {
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
 
-    // TODO: Add input validation
+    private TimePickerDialog createTimePicker(final TextView tv){
+        Calendar cal = Calendar.getInstance();
+        int hour = cal.get(Calendar.HOUR_OF_DAY);
+        int minute = cal.get(Calendar.MINUTE);
+        TimePickerDialog timePicker;
+        timePicker = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int selectedHour, int selectedMinute) {
+                tv.setText(((selectedHour > 12) ? selectedHour % 12 : selectedHour) + ":" + (selectedMinute < 10 ? ("0" + selectedMinute) : selectedMinute) + " " + ((selectedHour >= 12) ? "PM" : "AM"));
+            }
+        }, hour, minute, false);
+        return timePicker;
+    }
 }
